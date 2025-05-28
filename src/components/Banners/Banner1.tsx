687fbe3ddd4e78a8847e3c47e691cc685143ce19
zezo-melo <robertomello404@gmail.com>
2025-05-28 18:23:14 -0300

    changes on banner

--- src/components/Banners/Banner1.tsx
+++ src/components/Banners/Banner1.tsx
@@ -37,7 +37,7 @@ const handleScroll = () => {
                 cursor 
                 cursorStyle="|" 
                 typeSpeed={50}
-                deleteSpeed={40}
+                deleteSpeed={1}
                 delaySpeed={2000}
               />
             </span>
